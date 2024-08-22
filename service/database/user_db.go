ad8368bfcf67af7523ac2ab43f4498b8437019f6
Antonio-Cordeiro <antoniocordeiro@hotmail.it>
2024-08-22 11:15:34 +0200

    created setmyusername file

--- service/database/user_db.go
+++ service/database/user_db.go
@@ -9,14 +9,17 @@ import (
 
 // GetName is an example that shows you how to query data
 func (db *appdbimpl) UserLogin(username string) (types.User, error) {
+	//try to insert the username into the databse, if it is already in the database do nothing, otherwise add it
 	_, err := db.c.Exec("INSERT INTO users(username, followers, following, postCount) VALUES (?,0,0,0) ON CONFLICT (username) DO NOTHING", username)
 	if err != nil {
 		return types.User{}, err
 	}
-	
+	//if it is already in the database take its data
 	var user types.User
 	if err := db.c.QueryRow("SELECT username, followers, following, postCount FROM users WHERE username = ?", username).Scan(&user.Username, &user.Followers, &user.Following, &user.PostCount); err != nil {
 		return types.User{}, err
 	}
 	return user, nil
 }
+
+
